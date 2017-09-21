92e596674a49c00c1a0c37edaba7073554b31040
朱青 <614128859@qq.com>
2017-09-21 16:54:12 +0800

    create QuDriver

---
+++ QuDriver/Pods/Target Support Files/Pods-QuDriver/Pods-QuDriver-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuDriver : NSObject
+@end
+@implementation PodsDummy_Pods_QuDriver
+@end
---
+++ QuDriver/QuDriver/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuDriver
+//
+//  Created by 朱青 on 2017/9/20.
+//  Copyright © 2017年 com.Qyueche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
